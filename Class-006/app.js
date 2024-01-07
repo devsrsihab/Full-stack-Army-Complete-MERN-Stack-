350ed80eba4c411203054b423a89e81caf202a97
devsrsihab <srsohankhan0@gmail.com>
2024-01-07 14:00:10 +0600

    Start The Journey

---
+++ Class-006/app.js
@@ -0,0 +1,22 @@
+const arr = [1, 2, 3, null, false, 4, 5, '', 'test', 6, 7];
+
+
+// for (let i = 0; i < arr.length  ; i++ ) {
+
+//    for(let j = i ; j < arr.length - 1 ; j++ ){
+//     if(!arr[j] || typeof arr[j] !== 'number' ){
+//         arr[j] = arr[ j + 1]
+//         arr[j + 1] = undefined
+//     }
+//    }
+
+
+// }
+ arr[ 2 + 1] = 'sohan'
+
+
+console.log(arr)
+
+
+//  step-1 first print the arr value with for
+//  step-2 make a condition if the data type not number then make undefined
